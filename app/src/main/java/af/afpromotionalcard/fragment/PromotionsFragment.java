4b7e59a018023f0504d896fd2d8000c118124241
gharraph <sherief@gharraph.com>
2016-05-05 00:40:48 -0400

    created the project and started with recyle view tests

---
+++ app/src/main/java/af/afpromotionalcard/fragment/PromotionsFragment.java
@@ -0,0 +1,7 @@
+package af.afpromotionalcard.fragment;
+
+/**
+ * Created by gharraph on 5/5/16.
+ */
+public class PromotionsFragment {
+}
---
+++ app/src/test/java/af/afpromotionalcard/fragment/PromotionsFragmentTest.java
@@ -0,0 +1,20 @@
+package af.afpromotionalcard.fragment;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import af.afpromotionalcard.BuildConfig;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gharraph on 5/5/16.
+ */
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class PromotionsFragmentTest {
+    private PromotionsFragment fragment;
+
+}
